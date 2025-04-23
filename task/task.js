fb6cd7b5ecdac2b09dac2a90e5d9875f1fbd37e9
yahyasheriif <yahyasabdallah21@gmail.com>
2025-04-23 07:17:51 +0200

    hope the last commit

--- task/task.js
+++ task/task.js
@@ -24,29 +24,36 @@ const studentSchema = new mongoose.Schema({
 });
 
 const Student = mongoose.model('Student', studentSchema);// schema --> class to initialize students 
+
 // 1.Add a New Student (Hardcoded):
 let newStudent = new Student({
     name: 'John Doe',
     age: 20,
     level: 'Undergraduate',
-    address: '123 Main St'
-});
+    address: '123 Main St',
+}).save();
 
 let mohammed = new Student({
     name: 'Mohammed',
     age: 22,
     level: 'Graduate',
-    address: '456 Elm St'
-});
+    address: '456 Elm St',
+}).save();
 
 let ali = new Student({
     name: 'Ali',
     age: 23,
     level: 'Postgraduate',
-    address: '789 Oak St'
-});
+    address: '789 Oak St',
+}).save();
 
 // 2.Add a New Student (From Request Body):
+// {
+//     "name":"muhammed",
+//     "age":17 , 
+//     "level":"1",
+//     "address":"23 ali street"
+// }
 app.post('/students', async (req, res) => {
     const data = new Student({
         name: req.body.name,
@@ -65,11 +72,12 @@ const doctorSchema = new mongoose.Schema({
     name: String,
     age: Number,
     phone: String,
-    id: String
+    
 });
 
 const Doctor = mongoose.model('Doctor', doctorSchema);// schema --> class to initialize doctors
 // 3.Add a New Doctor (From Query Parameters):
+// http://localhost:3000/doctors?name=John&age=30&phone=1234567890&id=1
 app.post('/doctors', async (req, res) => {
     const {name, age, phone} = req.query;
     const data = new Doctor({
@@ -84,6 +92,7 @@ app.post('/doctors', async (req, res) => {
 });
 
 //4.Fetch All Students:
+// http://localhost:3000/students
 app.get('/students', async (req, res) => {
     const students = await Student.find();
     res.json(students);
@@ -91,30 +100,27 @@ app.get('/students', async (req, res) => {
 );
 
 // 5. Delete a Student:
+// http://localhost:3000/students/3
 app.delete('/students/:id', async (req, res) => {
     const id = req.params.id;
     const student = await Student.findById(id);
     if (!student) {
         return res.status(404).send('Student not found');
     }
-    await student.remove();
+    await Student.deleteOne({ _id: id });
     res.status(200);
     res.send("Student deleted successfully");
 }
 );
 // 6. Update a Doctor's Name:
-app.put('/doctors/:id', async (req, res) => {
-    const id = req.params.id;
-    const newName = req.body.name;
-    const newPhone = req.body.phone;
-    const newAge = req.body.age;
-    const doctor = await Doctor.findById(id);
+// http://localhost:3000/doctors?oldName=John&newName=John Doe
+app.put('/doctors/', async (req, res) => {
+    const{oldName,newName} = req.query;
+    const doctor = await Doctor.findOne({ name: oldName });
     if (!doctor) {
         return res.status(404).send('Doctor not found');
     }
     doctor.name = newName;
-    doctor.phone = newPhone;
-    doctor.age = newAge;
     await doctor.save();
     res.json(doctor);
     res.status(200);
@@ -123,6 +129,7 @@ app.put('/doctors/:id', async (req, res) => {
 }
 );
 // 7. Fetch Both Lists (Students and Doctors):
+// http://localhost:3000/all
 
 app.get("/all", async (req, res) => {
     const students = await Student.find();
